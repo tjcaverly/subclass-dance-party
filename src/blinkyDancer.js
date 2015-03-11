8cfa6685908a5ebe152db203c8a7ba29317864bd
MKS-Student <mks.student@hackreactor.com>
2015-03-10 19:51:28 -0700

    Add switch prof picture functionality.

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,13 +1,18 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps){
   //this.oldStep = makeDancer.prototype.step;
+  this.imageURLs = ['prof.png', 'youngprof.png'];
+  this.imageNum = 1;
+  var context = this;
   makeDancer.call(this, top, left, timeBetweenSteps);
   this.$node.addClass('blinky');
   this.$node.html("<img src='prof.png' height=75 width=60>");
-  this.$node.click(function(){
-    console.log($(this));
 
-    $(this).css("border", "30px solid orange");
-    $(this).css("border-radius", "30px");
+  this.$node.click(function(){
+    var imageURL = context.imageURLs[context.imageNum];
+    context.imageNum = (context.imageNum+1)%2;
+    $(this).html("<img src="+imageURL+" height=75 width=60>");
+    //$(this).css("border", "30px solid orange");
+    //$(this).css("border-radius", "30px");
     //console.log('hi');
   });
   //var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
